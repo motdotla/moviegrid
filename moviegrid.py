60cc60f906ed0edf6e294e72653320b2a1fb4cc4
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-07-13 16:26:33 -0700

    added hello world test for hbo endpoint

--- moviegrid.py
+++ moviegrid.py
@@ -1,5 +1,14 @@
 from flask import Flask
+from flask.ext import restful
+
 app = Flask(__name__)
+api = restful.Api(app)
+
+class HBO(restful.Resource):
+  def get(self):
+    return {'hello': 'world'}
+
+api.add_resource(HBO, '/hbo')
 
 @app.route("/")
 def index():
