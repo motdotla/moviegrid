0faa7c3a52887fe660b8368a1567ae34e04555c5
scottmotte <scott@scottmotte.com>
2013-07-13 16:02:40 -0700

    Adds ls flask-restful

--- moviegrid.py
+++ moviegrid.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-  return "Hello World!"
+def index():
+  return "moviegrid"
+
+@app.route("/parse")
+def parse():
+  return "parse"
 
 if __name__ == "__main__":
   app.run()
