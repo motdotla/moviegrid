8072f3dfdfdabbce991e28e14d8e40e747fb48b0
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-07-13 20:45:34 -0700

    debugging

--- moviegrid.py
+++ moviegrid.py
@@ -14,15 +14,16 @@ class HBO(restful.Resource):
     parser.add_argument('name', type=str, required=True, help="Name cannot be blank!")
     args = parser.parse_args()
     url = "http://catalog.lv3.hbogo.com/apps/mediacatalog/rest/searchService/HBO/search?term=" + args['name']
-    r = requests.get(url)
-    root = ET.fromstring(r.text)
-    movie_name = root.findtext("body/results/promotionResponse/title", default="NA")
-    T_key = root.findtext("body/results/promotionResponse/TKey", default="NA")
-    link = "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=ted/video&assetID=" + T_key + "?videoMode=embeddedVideo?showSpecialFeatures=false"
-    if movie_name == "NA":
-        return { "message": "error", "errors": [ "Movie not found:" + url ] }
-    else:
-        return {'title': movie_name, 'url': link }
+    return url
+    # r = requests.get(url)
+    # root = ET.fromstring(r.text)
+    # movie_name = root.findtext("body/results/promotionResponse/title", default="NA")
+    # T_key = root.findtext("body/results/promotionResponse/TKey", default="NA")
+    # link = "http://www.hbogo.com/#search&browseMode=browseGrid?searchTerm=ted/video&assetID=" + T_key + "?videoMode=embeddedVideo?showSpecialFeatures=false"
+    # if movie_name == "NA":
+    #    return { "message": "error", "errors": [ "Movie not found:" + url ] }
+    # else:
+    #    return {'title': movie_name, 'url': link }
 
 api.add_resource(HBO, '/hbo')
 
