503d28dd9754b5a9bf1440e12f5588921707d443
Elmer Thomas <elmer.thomas@sendgrid.com>
2013-07-13 19:28:01 -0700

    adding parameter parsing

--- moviegrid.py
+++ moviegrid.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext import restful
+from flask.ext.restful import reqparse
 import requests
 import xml.etree.ElementTree as ET
 
@@ -8,7 +9,10 @@ api = restful.Api(app)
 
 class HBO(restful.Resource):
   def get(self):
-    movie_name = "ted"
+    parser = reqparse.RequestParser()
+    parser.add_argument('name', type=str, required=True, help="Name cannot be blank!")
+    args = parser.parse_args()
+    movie_name = args['name']
     r = requests.get("http://catalog.lv3.hbogo.com/apps/mediacatalog/rest/searchService/HBO/search?term=" + movie_name)
     root = ET.fromstring(r.text)
     movie_name = root.findtext("body/results/promotionResponse/title", default="NA")
