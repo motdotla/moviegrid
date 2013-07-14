5edc63b8d24911e5d02714de444cbde5ea661982
scottmotte <scott@scottmotte.com>
2013-07-14 13:13:18 -0700

    Load environment variables from .env

--- moviegrid.py
+++ moviegrid.py
@@ -6,9 +6,15 @@ from flask import Flask, Response, request
 import xml.etree.ElementTree as ET
 import urllib
 import simplejson
+import os
 
-app = Flask(__name__)
-api = restful.Api(app)
+app       = Flask(__name__)
+api       = restful.Api(app)
+TO        = os.environ.get('SENDGRID_TO')
+FROM      = os.environ.get('SENDGRID_FROM')
+SUBJECT   = os.environ.get('SENDGRID_SUBJECT')
+API_USER  = os.environ.get('SENDGRID_USERNAME')
+API_KEY   = os.environ.get('SENDGRID_PASSWORD')
 
 class HBO(restful.Resource):
   def get(self):
@@ -31,7 +37,7 @@ api.add_resource(HBO, '/hbo')
 
 @app.route("/")
 def index():
-  return "moviegrid"
+  return os.environ.get('HELLO')
 
 @app.route("/parse", methods=['GET', 'POST'])
 def sendgrid_parser():
@@ -44,7 +50,15 @@ def sendgrid_parser():
         to_address = envelope['to'][0]
         from_address = envelope['from']
         subject = request.form.get('subject')
-        payload = {'to': 'elmer@thinkingserious.com', 'from': 'hackers@sendgrid.com', 'subject': 'MovieGrid Results', 'text': from_address, 'html': from_address, 'api_user': 'hollywoodhackday', 'api_key': 'Kq8<bDE6FA'}
+        payload = {
+            'to': TO, 
+            'from': FROM, 
+            'subject': SUBJECT, 
+            'text': from_address, 
+            'html': from_address, 
+            'api_user': SENDGRID_USERNAME, 
+            'api_key': SENDGRID_PASSWORD 
+        }
         r = requests.get("http://sendgrid.com/api/mail.send.json", params=payload)
 
         return "HTTP/1.1 200 OK"
